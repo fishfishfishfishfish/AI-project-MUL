4128324d1d1a89abb2e851992396927bfc88aca7
fishfishfishfishfish <619156737@qq.com>
2017-12-23 17:20:17 +0800

    train file

--- manipDataset.py
+++ manipDataset.py
@@ -1,97 +1,83 @@
 import string
 import numpy
+import random
 
 
 class WordVector(object):
-    def __init__(self, word_filter):
-        self.word_filter = word_filter
-        self.word_vector = []
+    def __init__(self):
         self.words_idf = {}
+        self.word_vectors = []
         self.file_size = 0
-        self.file_lines = []
 
     def get_word_vector(self, lines):
         self.file_size = len(lines)
         for words in lines:
             for word in set(words):
-                if word not in self.word_filter and word != '':
-                    if word not in self.words_idf:
-                        self.words_idf[word] = 1
-                        self.word_vector.append(word)
-                    else:
-                        self.words_idf[word] += 1
+                if word not in self.words_idf:
+                    self.words_idf[word] = 1
+                    self.word_vectors.append(word)
                 else:
-                    while word in words:
-                        words.remove(word)
-            self.file_lines.append(words)
+                    self.words_idf[word] += 1
         for k, v in self.words_idf.items():
             self.words_idf[k] = self.file_size / (1 + v)
 
-    def cal_tfidf(self, line):
-        line_tfidf = [0 for t in range(len(self.word_vector))]
-        for word in set(line):
-            line_tfidf[self.word_vector.index(word)] = (line.count(word) / len(line) * self.words_idf[word])
+    def split_word_vector(self, split_amount):
+        temp_vec = self.word_vectors.copy()
+        self.word_vectors = []
+        split_size = len(temp_vec)//split_amount
+        for i in range(split_amount-1):
+            split_vec = []
+            for j in range(split_size):
+                split_vec.append(temp_vec.pop(random.randint(0, len(temp_vec)-1)))
+            self.word_vectors.append(split_vec)
+        self.word_vectors.append(temp_vec)
+
+    def cal_tfidf(self, line, word_vec_index):
+        word_vec = self.word_vectors[word_vec_index]
+        line_tfidf = [0 for t in range(len(word_vec))]
+        for word in word_vec:
+            line_tfidf[word_vec.index(word)] = (line.count(word) / len(line) * self.words_idf[word])
         return line_tfidf
 
 
-def read_train_file(filename):
+def read_text(filename):
     f = open(filename, 'r')
-    labels = []
     lines = []
     for line in f.readlines():
         line = line.strip('\n')
-        temp = line.split('\t\t')
-        temp[1] = remove_punctuation(temp[1])
-        words = temp[1].split(' ')
+        words = line.split(' ')
         while '' in words:
             words.remove('')
         lines.append(words)
-        if temp[0] == "LOW":
-            labels.append("1,0,0")
-        elif temp[0] == "MID":
-            labels.append("0,1,0")
-        elif temp[0] == "HIG":
-            labels.append("0,0,1")
-        else:
-            print("label go wrong!")
-    return lines, labels
+    return lines
 
 
-def write_train_file(filename, labels, wv):
+def write_file(filename, wv, lines, word_vec_index):
     outfile = open(filename, 'w')
     cnt = 0
-    for line in wv.file_lines:
-        print(cnt)
+    word_vec = wv.word_vectors[word_vec_index]
+    for w in word_vec:
+        outfile.write(w+' ')
+    outfile.write('\n')
+    for line in lines:
         cnt += 1
-        line_tfidf = wv.cal_tfidf(line)
+        if cnt % 100 == 0:
+            print(cnt)
+        line_tfidf = wv.cal_tfidf(line, word_vec_index)
         for t in line_tfidf:
             outfile.write(str(t)+',')
         outfile.write('\n')
 
 
-def remove_punctuation(s):
-    blanks = ""
-    for i in range(32):
-        blanks += " "
-    table = bytes.maketrans(bytes(string.punctuation, encoding='utf-8'), bytes(blanks, encoding='utf-8'))
-    s = s.translate(table)
-    return s
-
-
-FileName = "MulLabelTrain.ss"
-OutFileName = "result.csv"
-# OutFile = open(OutFileName, 'w')
-Lines, Labels = read_train_file(FileName)
-WV = WordVector(['sssss'])
+TrainTextFileName = "text_train_out_withoutend.txt"
+TestTextFileName = "text_test_out_withoutend.txt"
+TrainIDFFileName = "Train_TFIDF.csv"
+TestIDFFileName = "Test_TFIDF.csv"
+Lines = read_text(TrainTextFileName)
+WV = WordVector()
 WV.get_word_vector(Lines)
-print("got word vector")
-# Mat = WV.cal_tfidf()
-# print("got matrix")
-# for i in WV.word_vector:
-#     OutFile.write(i+',')
-# for i in range(len(Labels)):
-#     OutFile.write(Labels[i] + ' ')
-#     for n in Mat[i]:
-#         OutFile.write(str(n)+',')
-#     OutFile.write('\n')
-write_train_file(OutFileName, Labels, WV)
+WV.split_word_vector(20)
+write_file(TrainIDFFileName, WV, Lines, 0)
+print("got Train")
+Lines = read_text(TestTextFileName)
+write_file(TestIDFFileName, WV, Lines, 0)
